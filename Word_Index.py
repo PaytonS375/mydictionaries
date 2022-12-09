ec542ac23edc42caf5114fba214f8ac6f0419847
Payton <payton.sanislow@gmail.com>
2022-12-08 18:07:20 -0600

    final ec update

--- Word_Index.py
+++ Word_Index.py
@@ -3,21 +3,18 @@
 # each of these words show up in the text - 'Father', 'God', 'Christ', 'Spirit', 'life', 'man' (case sensitive).
 # (note: all punctuation marks, chapter and verse references have been removed to allow to analyze each word)
 
-import csv
-
 infile = open('book of John text.txt','r')
 
 words = {}
+words['words2'] = [" Father "," God ", " Christ ", " Spirit "," spirit "," life "," man "]
 
 for w in infile:
 
     for t in w.split():
+        t = " " + t + " "
         words[t] = w.count(t)
 
 for word in words:
-    print('Father: ' + str(words[word]))
-    print('God: ' + str(words[word]))
-    print('Christ: ' + str(words[word]))
-    print('Spirit: ' + str(words[word]))
-    print('life: ' + str(words[word]))
-    print('man: ' + str(words[word]))
+
+    if word in words['words2']:
+        print(word + ':', words[word])
